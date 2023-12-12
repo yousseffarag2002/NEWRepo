a64c84bdd87190d11fe70ae910ab0858a4c07918
Basem <Basem@gmail.com>
2023-12-12 14:39:16 +0300

    1st change on Remote repo

---
+++ koko.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int sum (int a , int b);
+void main() {
+	cout << "Hello world";
+	cout << "Hello ";
+	cout << "123";
+	cout << "cringe";
+	cout << "144444423";
+	cout << "ANAAAAAAAAA";
+}
+
+int sum (int a , int b){
+	return a + b ;
+}
